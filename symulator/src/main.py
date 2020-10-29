732ef24c0315ff126580b58a0bd613f1ffd436de
Magdalena Wilk <wilk1609@gmail.com>
2020-10-29 12:50:00 +0100

    bugfix w algorytmie FCFS

--- symulator/src/main.py
+++ symulator/src/main.py
@@ -78,7 +78,7 @@ class Event(object):
         self.process_id = process_id
 
 
-def calculate_statistics(process_list : List[Process], file=False, filename="stats.txt"):
+def calculate_statistics(process_list: List[Process], file=False, filename="stats.txt"):
     waiting_time = 0.0 # czas opoznienia
     turn_around_time = 0.0 # czas odpowiedz
     done_in_time_count = 0
@@ -100,12 +100,22 @@ def basic_fcfs(processes: Processes):
     process_list = processes.process_list
     time = process_list[0].start_time
     delay = 0
+    total_idle_time = 0
+    finish_time = 0
     for proc in process_list:
         time += delay
+        idle_time = proc.start_time - time
+        if idle_time > 0:
+            events.append(Event(time, -1))
+            time += idle_time
+            total_idle_time += idle_time
         events.append(Event(time, proc.process_id))
         proc.processing_start = time
         proc.processing_end = time + proc.duration
+        finish_time = proc.processing_end
         delay = proc.duration
+
+    print("Idle time:", round(total_idle_time,2), "Load:", round(1.0-total_idle_time/finish_time, 2), "%")
 
     print("EVENTS:\nprocess_id processing_start")
     for ev in events:
