6f7f4307e73a17463bc627c9e6cb1c8cf8a2a9ff
Magdalena Wilk <wilk1609@gmail.com>
2020-12-17 22:35:34 +0100

    drawing plots updated

--- symulator/src/TestResults.py
+++ symulator/src/TestResults.py
@@ -23,10 +23,9 @@ class TestsResults(object):
     test_list: List[Test]
     df: pd.DataFrame
 
-    def __init__(self, intervals):
+    def __init__(self):
         self.test_list = []
         self.df = None
-        self.intervals = intervals
 
     def append(self, test_result):
         self.test_list.append(test_result)
@@ -35,8 +34,9 @@ class TestsResults(object):
         for test in self.test_list:
             print(test.to_string())
 
-    def read_from_file(self):
-        pass
+    def read_from_file(self, filename):
+        df = pd.read_csv(filename)
+        self.df = df.groupby(["filename", "alg_type"]).mean().reset_index()
 
     def to_latex_table(self):
         pass
@@ -54,42 +54,50 @@ class TestsResults(object):
 
         return self.df
 
-    def do_plot(self, df, variables, type=0, to_file=True, show=False, const_param="cv", const_param_value=1, x_var="wl", output_path=""):
+    def do_plot(self, df, variables, type=0, to_file=True, show=False, const_param="cv", const_param_values=[1], x_var="wl", output_path=""):
+        variables = ['avg_late', 'avg_latency', 'done_in_time_percent']
         titles = ['Średni czas opóźnienia', 'Średni czas odpowiedzi', 'Zadania obsłużone w czasie [%]']
-        intervals_number = len(self.intervals)
-        fig, big_axes = plt.subplots(figsize=(5.0*intervals_number, 15.0), nrows=3, ncols=1, sharey=True)
+        const_param_values_len = len(const_param_values)
+        fig, big_axes = plt.subplots(figsize=(15.0, const_param_values_len * 5.0), nrows=const_param_values_len,
+                                     ncols=1, sharey=True)
 
         for row, big_ax in enumerate(big_axes, start=1):
-            big_ax.set_title(titles[row - 1] + "\n", fontsize=16)
+            if x_var is "wl":
+                big_ax.set_title("Współczynnik zmienności: " + str(const_param_values[row - 1]) + "\n", fontsize=16)
+            else:
+                big_ax.set_title("Obciążenie: " + str(const_param_values[row - 1]) + "\n", fontsize=16)
             big_ax.tick_params(labelcolor=(1., 1., 1., 0.0), top='off', bottom='off', left='off', right='off')
             big_ax._frameon = False
-
+        if x_var is "wl":
+            plot_kind = "scatter"
+        else:
+            plot_kind = "line"
         pos = 1
-        for var in variables:
-            for interval in self.intervals:
-                ax = fig.add_subplot(3, intervals_number, pos)
-                ax.set_title(str(interval) + " interwałów")
-                # ax.set_ylim(bottom=0)
-                if type == 0:
-                    df[(df["alg_type"] == 0) & (df["interval_number"] == interval)].plot(kind='line', x=x_var, y=var,
-                                                                                         color='red', ax=ax,
-                                                                                         label="Algorytm podstawowy")
-                    df[(df["alg_type"] == 1) & (df["interval_number"] == interval)].plot(kind='line', x=x_var, y=var,
-                                                                                         color='blue', ax=ax,
-                                                                                         label="Algorytm ulepszony")
-                else:
-                    df[(df["interval_number"] == interval)].plot(kind='line', x=x_var, y=var, color='black', ax=ax,
-                                                               label="Różnica między algorytmem ulepszonym, a podstawowym")
-                pos += 1
-        fig.set_facecolor('w')
+        for cpv in const_param_values:
+            for i in range(3):
+                title = titles[i]
+                var = variables[i]
+                ax = fig.add_subplot(const_param_values_len, 3, pos)
+                ax.set_title(title)
+                df[(df["alg_type"] == 0) & (df[const_param] == cpv)].plot(kind='line', x=x_var, y=var, color='red', ax=ax,
+                                                                   label="Algorytm podstawowy")
+                df[(df["alg_type"] == 1) & (df[const_param] == cpv)].plot(kind='line', x=x_var, y=var, color='blue', ax=ax,
+                                                                   label="Algorytm ulepszony")
+                df[(df["alg_type"] == 2) & (df[const_param] == cpv)].plot(kind='line', x=x_var, y=var, color='green', ax=ax,
+                                                                   label="Algorytm podstawowy, const dt_max")
+                df[(df["alg_type"] == 3) & (df[const_param] == cpv)].plot(kind='line', x=x_var, y=var, color='pink', ax=ax,
+                                                                   label="Algorytm ulepszony, const dt_max")
 
+                pos += 1
+
+        fig.set_facecolor('w')
         plt.tight_layout()
 
         if to_file:
-            if type == 1:
-                output_filename = output_path + const_param + '-' + str(const_param_value) + '.png'
+            if type == 0:
+                output_filename = name = f'{output_path}{const_param}-{const_param_values[0]}-{const_param_values[-1]}.png'
             else:
-                output_filename = output_path + 'diff-' + const_param + '-' + str(const_param_value) + '.png'
+                output_filename = name = f'{output_path}diff-{const_param}-{const_param_values[0]}-{const_param_values[-1]}.png'
             plt.savefig(output_filename)
         if show:
             plt.show()
@@ -105,9 +113,10 @@ class TestsResults(object):
         variables = ['avg_late 1-0', 'avg_latency 1-0', 'done_in_time_percent 1-0']
         self.do_plot(df, variables, 1, to_file, show, const_param, const_param_value, x_var, output_path)
 
-    def plot_results(self, to_file=True, show=False, const_param="cv", const_param_value=1, x_var="wl", output_path=""):
+    def plot_results(self, to_file=True, show=False, const_param="cv", const_param_values=[1], x_var="wl", output_path=""):
         if self.df is None:
             self.to_data_frame()
-        df = self.df
+        df = self.df.sort_values(x_var)
         variables = ['avg_late', 'avg_latency', 'done_in_time_percent']
-        self.do_plot(df, variables, 0, to_file, show, const_param, const_param_value, x_var, output_path)
+
+        self.do_plot(df, variables, 0, to_file, show, const_param, const_param_values, x_var, output_path)
